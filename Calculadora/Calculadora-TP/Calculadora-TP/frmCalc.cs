ad4780aa58a2ddce0cb04ae4256671a1072355be
Quintanilhaigor <48369364+Quintanilhaigor@users.noreply.github.com>
2019-06-28 21:40:48 -0300

    Add files via upload

---
+++ Calculadora/Calculadora-TP/Calculadora-TP/frmCalc.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculadora_TP
+{
+    public partial class frmCalc : Form
+    {
+        public frmCalc()
+        {
+            InitializeComponent();
+        }
+
+        private void frmCalc_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnMulti_Click(object sender, EventArgs e)
+        {
+            Calculadora calculadora = new Calculadora();
+
+            calculadora.Numero1 = Double.Parse(txtN1.Text);
+            calculadora.Numero2 = Double.Parse(txtN2.Text);
+            txtResultado.Text = Convert.ToString(calculadora.Multiplica());
+        }
+
+        private void btnExpo_Click(object sender, EventArgs e)
+        {
+            Calculadora calculadora = new Calculadora();
+
+            calculadora.Numero1 = Double.Parse(txtN1.Text);
+            calculadora.Numero2 = Double.Parse(txtN2.Text);
+            txtResultado.Text = Convert.ToString(calculadora.Expo());
+        }
+
+        private void btnSoma_Click(object sender, EventArgs e)
+        {
+
+
+            //Instanciação Tornar a classe um objeto (real)
+            Calculadora calculadora = new Calculadora();
+
+           calculadora.Numero1 = Double.Parse(txtN1.Text);
+           calculadora.Numero2 = Double.Parse(txtN2.Text);
+           txtResultado.Text = Convert.ToString(calculadora.Soma());         
+
+        }
+
+        private void btnSubtrai_Click(object sender, EventArgs e)
+        {
+            Calculadora calculadora = new Calculadora();
+            calculadora.Numero1 = Convert.ToDouble(txtN1.Text);
+            calculadora.Numero2 = Convert.ToDouble(txtN2.Text);
+            txtResultado.Text = Convert.ToString(calculadora.Subtrai());
+
+        }
+
+        private void btnDiv_Click(object sender, EventArgs e)
+        {
+            Calculadora calculadora = new Calculadora();
+            calculadora.Numero1 = Convert.ToDouble(txtN1.Text);
+            calculadora.Numero2 = Convert.ToDouble(txtN2.Text);
+            txtResultado.Text = Convert.ToString(calculadora.Div());
+        }
+    }
+}
---
+++ Calculadora/Calculadora.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora_TP
+{
+    class Calculadora
+    {
+      double numero1;
+      double numero2;
+
+        //getters e setters
+        public double Numero1 { get => numero1; set => numero1 = value; }
+        public double Numero2 { get => numero2; set => numero2 = value; }
+
+         public double Soma()
+        {
+          double resultado = Numero1 + Numero2;
+          return (resultado);
+        }
+        
+        public double Subtrai()
+        {
+          double resultado = this.Numero1 - this.Numero2;
+            return (resultado);  
+        }
+
+        public double Multiplica()
+        {
+            double resultado = this.Numero1 * this.Numero2;
+            return (resultado);
+        }
+
+        public double Div()
+        {
+            double resultado = this.Numero1 / this.Numero2;
+            return (resultado);
+        }
+
+        public double Expo()
+        {
+            double resultado = Math.Pow(this.Numero1, this.Numero2);
+            return (resultado);
+        }
+    }
+}
